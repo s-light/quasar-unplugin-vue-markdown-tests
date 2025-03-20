f22e83f81faedef2c909ccac0b832bef1afc3f71
s-light <git@s-light.eu>
2025-03-20 02:21:24 +0100

    test with relative paths

--- src/components/markdown-it-plugins/markdown-it-plugin-abbr-as-mdabbr.js
+++ src/components/markdown-it-plugins/markdown-it-plugin-abbr-as-mdabbr.js
@@ -7,7 +7,8 @@
 
 // export const mksAbbrCollection = {}
 
-import md2html from "src/components/markdown-it-plugins/markdown-rendering.js";
+// import md2html from "src/components/markdown-it-plugins/markdown-rendering.js";
+import md2html from "./markdown-rendering.js";
 
 // import { glob } from 'node:fs/promises'
 import fs from "node:fs";
