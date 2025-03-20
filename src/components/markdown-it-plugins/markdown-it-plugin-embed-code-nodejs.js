d199b82ace2af3c62e0871cbc6153416903b2073
s-light <git@s-light.eu>
2025-03-20 02:42:52 +0100

    try embed as node plugin

---
+++ src/components/markdown-it-plugins/markdown-it-plugin-embed-code-nodejs.js
@@ -0,0 +1,134 @@
+// based & inspired by
+// https://github.com/seanWLawrence/markdown-it-plugin-data-src/blob/master/lib/index.js
+// https://github.com/camelaissani/markdown-it-include
+// https://github.com/tokusumi/markdown-embed-code
+
+import fs from "node:fs";
+import path from "node:path";
+
+// check with https://regex101.com/
+// c++ :./relative/file/to/your/code.cpp
+// const RE_INFO = /(?<codeLang>.*?)\s?\:(?<codeFilePath>.*)/;
+const RE_INFO = /(?<codeLang>.*?)\s?:(?<codeFilePath>.*)/;
+
+/**
+ * embed code from relative file paths
+ * example:
+ * ```md
+ * ```c++ :./relative/file/to/your/code.cpp
+ *  // this code here is overwritten..
+ *  // this way you can embed a info here for renderer that do not understand the embedding..
+ *  //something like:
+ *
+ *  // please look at ./relative/file/to/your/code.cpp
+ * ```
+ * @module embedCode
+ * @param {MarkdownItTokens} tokens - MarkdownIt instance
+ * @param {MarkdownItOptions} options - options Object
+ * @param {Object} env - environment options Object
+ * @author Stefan Krüger s-light.eu
+ * @version 1.0.0
+ * @license MIT
+ * @exports embedCode
+ */
+// const embedCode = async (tokens, idx, options, env, self) => {
+const embedCode = (tokens, idx, options, env) => {
+    // console.group(`embedCode: idx [${idx}]~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~`);
+    const token = tokens[idx];
+    // console.log(`token in: `, token);
+    if (token.meta == undefined) {
+        token.meta = {};
+    }
+    // console.log(`options: `, options);
+    // console.log(`env: `, env);
+    const reResult = RE_INFO.exec(token.info);
+    // console.log(`reResult: `, reResult);
+    if (reResult) {
+        const { codeLang, codeFilePath } = reResult.groups;
+        token.meta.codeLanguage = codeLang;
+        token.info = codeLang;
+        token.meta.codeFilePath = codeFilePath;
+        // console.log(`codeLang: `, codeLang);
+        // console.log(`codeFilePath: `, codeFilePath);
+        if (codeFilePath) {
+            // found code file to include.
+            // let us first try to change relative path to absolut:
+            const fullPath = env.id;
+            const basePath = path.dirname(fullPath);
+            // console.log("basePath", basePath);
+            const filePath = codeFilePath.replace("./", basePath + path.sep);
+            token.meta.filePath = filePath;
+            // console.log(`filePath: `, filePath);
+            let codeContent = undefined;
+            try {
+                // codeContent = await fetch(filePath).then(async (response) => {
+                //     console.log(`response: `, response);
+                //     if (response.status == 200) {
+                //         return await response.text();
+                //     } else if (response.status == 404) {
+                //         throw new Error(`embed failed: file '${filePath}' not found.`, response);
+                //     } else {
+                //         throw new Error(`embed failed.`, response);
+                //     }
+                // });
+                if (fs.existsSync(filePath)) {
+                    codeContent = fs.readFileSync(filePath, "utf8");
+                    token.meta.fileExists = true;
+                } else {
+                    // throw new Error(`embed failed: file '${filePath}' not found.`);
+                    console.log(`embed failed: file '${filePath}' not found.`);
+                    token.meta.fileExists = false;
+                }
+                // console.log(`codeContent: `, codeContent);
+            } catch (error) {
+                console.log(error);
+            }
+            if (codeContent) {
+                // console.log("overwrite token.content.");
+                token.content = codeContent;
+                // console.log("token", token);
+            }
+        }
+    } else {
+        token.meta.codeLanguage = token.info;
+    }
+    // console.log("token out", token);
+    // console.groupEnd()
+};
+
+/**
+ * embed code from relative file paths
+ * example:
+ * ```md
+ * ```c++ :./relative/file/to/your/code.cpp
+ *  // this code here is overwritten..
+ *  // this way you can embed a info here for renderer that do not understand the embedding..
+ *  //something like:
+ *
+ *  // please look at ./relative/file/to/your/code.cpp
+ * ```
+ * @module mditPluginEmbedCodeNodejs
+ * @param {MarkdownItTokens} tokens - MarkdownIt instance
+ * @param {MarkdownItOptions} options - options Object
+ * @param {Object} env - environment options Object
+ * @param {MarkdownIt} self - MarkdownIt instance
+ * @returns {undefined} - Side effects only
+ * @author Stefan Krüger s-light.eu
+ * @version 1.0.0
+ * @license MIT
+ * @exports mditPluginEmbedCodeNodejs
+ */
+export function mditPluginEmbedCodeNodejs(md) {
+    const defaultRender =
+        md.renderer.rules.fence ||
+        function (tokens, idx, options, env, self) {
+            return self.renderToken(tokens, idx, options);
+        };
+
+    md.renderer.rules.fence = function (tokens, idx, options, env, self) {
+        embedCode(tokens, idx, options, env);
+        return defaultRender(tokens, idx, options, env, self);
+    };
+}
+
+export default mditPluginEmbedCodeNodejs;
